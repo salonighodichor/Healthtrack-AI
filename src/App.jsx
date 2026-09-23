117f88a0c107544abccccf5958d37b50da60df29
Vaishnavi Pohare <poharevaishnavi7@gmail.com>
2026-09-23 18:15:02 +0530

    Fix App.jsx merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-<<<<<<< HEAD
 import Login from "./pages/loginpages/login";
-=======
 import Emergency from "./pages/home/Emergency";
->>>>>>> 15b3f62 (Update home page and add emergency feature)
 import CreateAccount from "./pages/loginpages/CreateAccount";
 import Forgotpage from "./pages/loginpages/Forgotpage";
 
@@ -71,27 +68,12 @@ function App() {
           path="/caretaker-register"
           element={<CaretakerRegister />}
         />
-<<<<<<< HEAD
-=======
-<Route
-  path="/patient"
-  element={<Patient />}
-/>
-<Route
-  path="/doctor"
-  element={<Doctor />}
-/>
-<Route
-  path="/caretaker"
-  element={<Caretaker />}
-/> 
-<Route
-  path="/Emergency"
-  element={<Emergency />}
-/>
 
-      </Routes>
->>>>>>> 15b3f62 (Update home page and add emergency feature)
+        {/* Emergency */}
+        <Route
+          path="/Emergency"
+          element={<Emergency />}
+        />
 
         {/* ================= PATIENT ================= */}
 
